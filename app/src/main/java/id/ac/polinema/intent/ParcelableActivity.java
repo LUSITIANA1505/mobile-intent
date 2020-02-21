49686ade0ab08c30c32254f1b79a1d48e5e3314a
Lusi Tiana <lucytiana1504@gmail.com>
2020-02-21 11:20:47 +0700

    Praktikum_intent2

--- app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -1,15 +1,37 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import id.ac.polinema.intent.model.User;
+
 public class ParcelableActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText name;
+    private EditText age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        username = findViewById(R.id.input_username);
+        name = findViewById(R.id.input_name);
+        age = findViewById(R.id.input_age);
     }
 
+    public void handleParcel2(View view) {
+        String username1 = username.getText().toString();
+        String name1 = name.getText().toString();
+        int age1 = Integer.parseInt(age.getText().toString());
+
+        User u = new User(username1, name1, age1);
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra("user", u);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -1,10 +1,16 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import id.ac.polinema.intent.model.User;
+
 public class ProfileParcelableActivity extends AppCompatActivity {
+    private TextView usernameTampil;
+    private TextView nameTampil;
+    private TextView ageTampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +19,19 @@ public class ProfileParcelableActivity extends AppCompatActivity {
 
         // TODO: bind here
 
+        usernameTampil = findViewById(R.id.text_username);
+        nameTampil = findViewById(R.id.text_name);
+        ageTampil = findViewById(R.id.text_age);
+
         Bundle extras = getIntent().getExtras();
+        User us = extras.getParcelable("user");
         if (extras != null) {
 
             // TODO: display value here
+
+            usernameTampil.setText(us.getUsername());
+            nameTampil.setText(us.getName());
+            ageTampil.setText(String.valueOf(us.getAge()));
         }
     }
 }
